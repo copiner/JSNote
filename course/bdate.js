cd147532687b34dff0c236ad7b0b9f492d02c057
copiner <810023494@qq.com>
2018-09-26 09:06:10 +0800

    course

---
+++ course/bdate.js
@@ -0,0 +1,24 @@
+function getNowFormatDate(){
+    var day = new Date();
+    var Year = 0;
+    var Month = 0;
+    var Day = 0;
+    var CurrentDate = "";
+    Year= day.getFullYear();//Ö§³ÖIEºÍ»ğºüä¯ÀÀÆ÷.
+    Month= day.getMonth()+1;
+    Day = day.getDate();
+    CurrentDate += Year;
+    if (Month >= 10 ){
+     CurrentDate += Month;
+    }
+    else{
+     CurrentDate += "0" + Month;
+    }
+    if (Day >= 10 ){
+     CurrentDate += Day ;
+    }
+    else{
+     CurrentDate += "0" + Day ;
+    }
+    return CurrentDate;
+ }
